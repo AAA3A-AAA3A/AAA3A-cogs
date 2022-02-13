efe5bcd9594f58786f2b740e3ec97c5dd2b598cd
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-02-13 18:23:13 +0100

    Update tickettool.py

--- tickettool/tickettool.py
+++ tickettool/tickettool.py
@@ -52,8 +52,8 @@ class TicketTool(settings, commands.Cog):
                 "embed_button": {
                     "title": "Create a ticket",
                     "description": ( "To get help on this server or to place a command for example, you can create a ticket.\n",
-                                    f"Just use the command `{ctx.prefix}ticket create` or click on the button below.\n",
-                                    f"You can then use the `{ctx.prefix}ticket` subcommand to manage your ticket."),
+                                     "Just use the command `{prefix}ticket create` or click on the button below.\n",
+                                     "You can then use the `{prefix}ticket` subcommand to manage your ticket."),
                 },
             },
             "tickets": {},

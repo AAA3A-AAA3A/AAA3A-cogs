b306b8818036a4fbdd28f976a188a3042473b79d
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-02-25 11:46:33 +0100

    Update editfile.py

--- editfile/editfile.py
+++ editfile/editfile.py
@@ -79,7 +79,7 @@ class EditFile(commands.Cog):
 
     @editfile.command()
     async def listdir(self, ctx, *, path: Path):
-        """Get a file on the bot's host machine from its path.
+        """List all files/directories of a directory from a path.
         """
         if not path.exists():
             await ctx.send("This directory cannot be found on the host machine.")

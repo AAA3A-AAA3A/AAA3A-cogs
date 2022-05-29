f82ac8341f31bd32210ac4157c3c86b751755265
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-05-29 15:23:32 +0200

    [Medicat] Removed the guild check because it caused an error when the bot started.

--- medicat/__init__.py
+++ medicat/__init__.py
@@ -9,8 +9,8 @@ with open(Path(__file__).parent / "info.json") as fp:
     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot: Red):
-    await bot.wait_until_ready()
-    if bot.get_guild(MEDICAT_GUILD) is None and bot.get_guild(TEST_GUILD) is None:
-        raise RuntimeError(f"The cog Medicat is not intended for use by normal users. All functions are directly based on the id of a server.")
+    # await bot.wait_until_ready()
+    # if bot.get_guild(MEDICAT_GUILD) is None and bot.get_guild(TEST_GUILD) is None:
+    #     raise RuntimeError(f"The cog Medicat is not intended for use by normal users. All functions are directly based on the id of a server.")
     cog = Medicat(bot)
     await CogsUtils().add_cog(bot, cog)

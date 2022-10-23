36584876181ce0781c9d0a887b8b5d87f47907f9
AAA3A-AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2022-10-23 12:25:10 +0200

    Creation and modification of files necessary for the use of Black and Isort.

--- make.bat
+++ make.bat
@@ -1,26 +1,30 @@
 @echo off
 
+call %USERPROFILE%\redenv\Scripts\activate.bat
 if [%1] == [] goto help
 
-if exist "%~dp0.venv\" (
-    set "VENV_PYTHON=%~dp0.venv\Scripts\python"
-) else (
-    set VENV_PYTHON=python
-)
+REM This allows us to expand variables at execution
+setlocal ENABLEDELAYEDEXPANSION
 
 goto %1
 
 :reformat
-"%VENV_PYTHON%" -m isort "%~dp0." -m 7 -l 1000 -p redbot -s "cogsutils.py" --sg ".github/* --sg "docs/*" --sp "%~dp0.\.isort.cfg"
-goto:eof
+isort .
+black .
+exit /B %ERRORLEVEL%
 
-:stylecheck
-"%VENV_PYTHON%" -m isort --check "%~dp0."
-goto:eof
+:isort
+isort .
+exit /B %ERRORLEVEL%
+
+:black
+black .
+exit /B %ERRORLEVEL%
 
 :stylediff
-"%VENV_PYTHON%" -m isort --check --diff "%~dp0."
-goto:eof
+isort --atomic --check --diff --line-length 99 --use-parentheses .
+black --check --diff -l 99 .
+exit /B %ERRORLEVEL%
 
 :help
 echo Usage:
@@ -28,6 +32,6 @@ echo   make ^<command^>
 echo.
 echo Commands:
 echo   reformat                   Reformat all .py files being tracked by git.
-echo   stylecheck                 Check which tracked .py files need reformatting.
-echo   stylediff                  Show the post-reformat diff of the tracked .py files
-echo                              without modifying them.
+echo   isort                      Reformat all .py files only with isort.
+echo   black                      Reformat all .py files only with black.
+echo   stylediff                  Check .py files for style diffs.

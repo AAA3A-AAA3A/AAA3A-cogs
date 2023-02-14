43e7f1360cf162bc96db621f38fa5a5ab6ba7e4b
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-02-14 22:18:20 +0100

    [TicketTool] Fixed error.

--- tickettool/tickettool.py
+++ tickettool/tickettool.py
@@ -1,4 +1,4 @@
-wfrom .AAA3A_utils import CogsUtils, Settings  # isort:skip
+from .AAA3A_utils import CogsUtils, Settings  # isort:skip
 from redbot.core import commands  # isort:skip
 from redbot.core.i18n import Translator, cog_i18n  # isort:skip
 from redbot.core.bot import Red  # isort:skip

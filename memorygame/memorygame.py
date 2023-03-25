56aa27700409cc174945c517a2b02023b0c477ee
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-03-25 17:11:03 +0100

    [MemoryGame] Add max wrong matches, leaderboard and Red bank (ty Vert & Max).

--- memorygame/memorygame.py
+++ memorygame/memorygame.py
@@ -1,15 +1,20 @@
-﻿from .AAA3A_utils import Cog, CogsUtils  # isort:skip
-from redbot.core import commands  # isort:skip
+﻿from .AAA3A_utils import Cog, CogsUtils, Menu, Settings  # isort:skip
+from redbot.core import commands, Config  # isort:skip
 from redbot.core.i18n import Translator, cog_i18n  # isort:skip
 from redbot.core.bot import Red  # isort:skip
 import discord  # isort:skip
 import typing  # isort:skip
+
+from tabulate import tabulate
+
+from redbot.core.utils.chat_formatting import box, pagify
 
 from .view import MemoryGameView
 
 # Credits:
 # General repo credits.
 # Thanks to Flame for his tests which allowed to discover several errors!
+# Thanks to Vertyco for ideas, and leaderboard code (https://github.com/vertyco/vrt-cogs/blob/main/pixl/pixl.py)!
 
 _ = Translator("MemoryGame", __file__)
 
@@ -23,18 +28,133 @@ else:
 
 @cog_i18n(_)
 class MemoryGame(Cog):
-    """A cog to play to Memory game, with buttons!"""
+    """A cog to play to Memory game, with buttons, leaderboard and Red bank!"""
 
     def __init__(self, bot: Red) -> None:
         self.bot: Red = bot
 
+        self.config: Config = Config.get_conf(
+            self,
+            identifier=205192943327321000143939875896557571750,
+            force_registration=True,
+        )
+        self.memorygame_guild: typing.Dict[str, typing.Union[typing.Optional[int], bool]] = {
+            "max_wrong_matches": None,
+            "red_economy": False,
+            "max_prize": 5000,
+            "reduction_per_second": 5,
+            "reduction_per_wrong_match": 15,
+        }
+        self.memorygame_member: typing.Dict[str, int] = {
+            "score": 0,
+            "wins": 0,
+            "games": 0,
+        }
+        self.config.register_guild(**self.memorygame_guild)
+        self.config.register_member(**self.memorygame_member)
+
         self.games: typing.Dict[discord.Message, MemoryGameView] = {}
 
         self.cogsutils: CogsUtils = CogsUtils(cog=self)
+
+        _settings: typing.Dict[
+            str, typing.Dict[str, typing.Union[typing.List[str], bool, str]]
+        ] = {
+            "max_wrong_matches": {
+                "path": ["max_wrong_matches"],
+                "converter": int,
+                "description": "Set the maximum tries for each game. Use `0` for no limit.",
+            },
+            "red_economy": {
+                "path": ["red_economy"],
+                "converter": bool,
+                "description": "If this option is enabled, the cog will give credits to the user each time the game is won.",
+            },
+            "max_prize": {
+                "path": ["max_prize"],
+                "converter": int,
+                "description": "Set the max prize for Red bank system and cog leaderboard. Default is 5000.",
+            },
+            "reduction_per_second": {
+                "path": ["max_prize"],
+                "converter": int,
+                "description": "Set the reduction per second of prize for Red bank system and cog leaderboard. Default is 5.",
+            },
+            "reduction_per_wrong_match": {
+                "path": ["max_prize"],
+                "converter": int,
+                "description": "Set the reduction per wrong match of prize for Red bank system and cog leaderboard. Default is 15.",
+            },
+        }
+        self.settings: Settings = Settings(
+            bot=self.bot,
+            cog=self,
+            config=self.config,
+            group=self.config.GUILD,
+            settings=_settings,
+            global_path=[],
+            use_profiles_system=False,
+            can_edit=True,
+            commands_group=self.configuration,
+        )
+
+    async def cog_load(self):
+        await self.settings.add_commands()
 
     @hybrid_command()
     async def memorygame(self, ctx: commands.Context) -> None:
         """
         Play to Memory game.
         """
-        await MemoryGameView(cog=self).start(ctx)
+        if ctx.guild is not None:
+            max_wrong_matches = await self.config.guild(ctx.guild).max_wrong_matches()
+        else:
+            max_wrong_matches = None
+        await MemoryGameView(cog=self, max_wrong_matches=max_wrong_matches).start(ctx)
+
+    @hybrid_command()
+    async def memorygameleaderboard(self, ctx: commands.Context) -> None:
+        all_members = await self.config.all_members(ctx.guild)
+        all_members = {ctx.guild.get_member(member): data for member, data in all_members.items() if ctx.guild.get_member(member) is not None}
+        if not all_members:
+            raise commands.UserFeedbackCheckFailure(_("No one has played this game yet."))
+        sorted_members = sorted(all_members.items(), key=lambda x: x[1]["score"], reverse=True)
+        you = next(
+            (
+                f"You: {num}/{len(all_members)}"
+                for num, data in enumerate(sorted_members, start=1)
+                if data[0] == ctx.author
+            ),
+            None,
+        )
+        embeds = []
+        table = []
+        for num in range(len(sorted_members)):
+            place = num + 1
+            member: discord.Member = sorted_members[num][0]
+            data = sorted_members[num][1]
+            table.append([place, member.display_name if self.cogsutils.is_dpy2 else member.name, data["score"], data["wins"], data["games"]])
+        board = tabulate(
+            tabular_data=table,
+            headers=["#", "Name", "Score", "Wins", "Games"],
+            numalign="left",
+            stralign="left",
+        )
+        for page in pagify(board, page_length=2000):
+            embed = discord.Embed(title="MemoryGame Leaderboard")
+            embed.description = box(page, lang="py")
+            if you:
+                embed.set_footer(text=you)
+            embeds.append(embed)
+        await Menu(pages=embeds).start(ctx)
+
+    @commands.guild_only()
+    @commands.admin_or_permissions(administrator=True)
+    @hybrid_group(name="setmemorygame")
+    async def configuration(self, ctx: commands.Context) -> None:
+        """Group of commands to set MemoryGame."""
+        pass
+
+    @configuration.command()
+    async def resetleaderboard(self, ctx: commands.Context) -> None:
+        await self.config.clear_all_members(ctx.guild)
--- memorygame/view.py
+++ memorygame/view.py
@@ -6,14 +6,18 @@ import asyncio
 import random
 import time
 
+from redbot.core import bank
+from redbot.core.errors import BalanceTooHigh
+
 # GAME_EMOJIS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 GAME_EMOJIS = ["🏆", "🎯", "🎲", "⚽", "🏀", "🏓", "🥁", "🎮", "🎳", "🎻", "🎖️", "🏹"]
 
 class MemoryGameView(discord.ui.View):
-    def __init__(self, cog: commands.Cog) -> None:
+    def __init__(self, cog: commands.Cog, max_wrong_matches: typing.Optional[int] = None) -> None:
         super().__init__(timeout=60 * 10)
         self.ctx: commands.Context = None
         self.cog: commands.Cog = cog
+        self.max_wrong_matches: typing.Optional[int] = max_wrong_matches
 
         self._message: discord.Message = None
         self._solution: typing.List[str] = []
@@ -24,22 +28,14 @@ class MemoryGameView(discord.ui.View):
 
         self._start: typing.Optional[float] = None
         self._end: typing.Optional[float] = None
+        self._tries: int = 0
+        self._wrong_matches: int = 0
 
         self._lock: asyncio.Lock = asyncio.Lock()
-
+   
     async def start(self, ctx: commands.Context) -> None:
         self.ctx: commands.Context = ctx
-        self._solution = GAME_EMOJIS.copy() * 2
-        random.shuffle(self._solution)
-        raw = self._solution.copy()
-        raw.insert(12, "​")  # invisible character
-        self._solution_display = [
-            raw[:5],
-            raw[5:10],
-            raw[10:15],
-            raw[15:20],
-            raw[20:25]
-        ]
+        self._solution, self._solution_display = self.get_emojis()
         for _list in self._solution_display:
             for emoji in _list:
                 if emoji != "​":
@@ -71,12 +67,25 @@ class MemoryGameView(discord.ui.View):
         for child in self.children:
             child: discord.ui.Item
             if isinstance(child, discord.ui.Button) and child.style != discord.ButtonStyle.url and child.custom_id != "​":
-                child.label = self._custom_ids[child.custom_id]
                 child.disabled = True
         try:
             await self._message.edit(view=self)
         except discord.HTTPException:
             pass
+
+    def get_emojis(self) -> typing.Tuple[typing.List[str], typing.List[typing.List[str]]]:
+        solution = GAME_EMOJIS.copy() * 2
+        random.shuffle(solution)
+        raw = solution.copy()
+        raw.insert(12, "​")  # invisible character
+        solution_display = [
+            raw[:5],
+            raw[5:10],
+            raw[10:15],
+            raw[15:20],
+            raw[20:25]
+        ]
+        return solution, solution_display
 
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.defer()
@@ -89,6 +98,7 @@ class MemoryGameView(discord.ui.View):
                 button.label = self._custom_ids[self._selected]
                 self._message = await self._message.edit(view=self)
                 return
+            self._tries += 1
             if self._custom_ids[self._selected] != self._custom_ids[interaction.data["custom_id"]] or self._selected == interaction.data["custom_id"]:
                 button1: discord.ui.Button = discord.utils.get(self.children, custom_id=self._selected)
                 button2: discord.ui.Button = discord.utils.get(self.children, custom_id=interaction.data["custom_id"])
@@ -103,6 +113,9 @@ class MemoryGameView(discord.ui.View):
                 button2.label = "​"
                 self._message = await self._message.edit(view=self)
                 self._selected = None
+                self._wrong_matches += 1
+                if self.max_wrong_matches and self._wrong_matches >= self.max_wrong_matches:
+                    await self.lose()
                 return
             button1: discord.ui.Button = discord.utils.get(self.children, custom_id=self._selected)
             button2: discord.ui.Button = discord.utils.get(self.children, custom_id=interaction.data["custom_id"])
@@ -120,11 +133,46 @@ class MemoryGameView(discord.ui.View):
         self._selected = None
         for child in self._children:
             if isinstance(child, discord.ui.Button) and child.custom_id != "​":
+                child.label = self._custom_ids[child.custom_id]
                 child.style = discord.ButtonStyle.success
+                if self._custom_ids[child.custom_id] not in self._found:
+                    self._found.append(self._custom_ids[child.custom_id])
+        if self._tries == 0:
+            self._tries = len(self._found)
         self._end = time.monotonic()
+        game_time = int(self._end - self._start)
+        config = await self.cog.config.guild(self.ctx.guild).all()
+        max_prize = config["max_prize"]
+        reduction_per_second = config["reduction_per_second"]
+        reduction_per_wrong_match = config["reduction_per_wrong_match"]
+        member_config = await self.cog.config.member(self.ctx.author).all()
+        member_config["score"] += max_prize - (game_time * reduction_per_second) - (self._wrong_matches * reduction_per_wrong_match)
+        member_config["wins"] += 1
+        member_config["games"] += 1
+        await self.cog.config.member(self.ctx.author).set(member_config)
+        if self.cog.config.guild(self.ctx.guild).red_economy():
+            # https://canary.discord.com/channels/133049272517001216/133251234164375552/1089212578279997521
+            final_prize = max_prize - (game_time * reduction_per_second) - (self._wrong_matches * reduction_per_wrong_match)
+            try:
+                await bank.deposit_credits(self.ctx.author, final_prize)
+            except BalanceTooHigh as e:
+                await bank.set_balance(self.ctx.author, e.max_balance)
         embed: discord.Embed = discord.Embed(title="Memory Game", color=discord.Color.green())
         embed.set_author(name=self.ctx.author.display_name, icon_url=self.ctx.author.display_avatar)
-        embed.description = f"You won in {int(self._end - self._start)} seconds!"
+        embed.description = f"You won in {game_time} seconds, with {self._tries} tries and {self._wrong_matches} wrong matches!"
+        self._message = await self._message.edit(embed=embed, view=self)
+        await self.on_timeout()
+        self.stop()
+
+    async def lose(self):
+        self._selected = None
+        self._end = time.monotonic()
+        member_config = await self.cog.config.member(self.ctx.author).all()
+        member_config["games"] += 1
+        await self.cog.config.member(self.ctx.author).set(member_config)
+        embed: discord.Embed = discord.Embed(title="Memory Game", color=discord.Color.green())
+        embed.set_author(name=self.ctx.author.display_name, icon_url=self.ctx.author.display_avatar)
+        embed.description = f"You lose, because you tried too many times ({self._tries} tries and {self._wrong_matches} wrong matches)."
         self._message = await self._message.edit(embed=embed, view=self)
         await self.on_timeout()
         self.stop()

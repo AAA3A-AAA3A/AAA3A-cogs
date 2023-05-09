b02e3777c509f7c5aed6ed6299cbe69a09b66ccf
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-05-09 20:32:04 +0200

    [All] Update an error message.

--- dropdownstexts/__init__.py
+++ dropdownstexts/__init__.py
@@ -11,7 +11,7 @@ needed_utils_version = data["needed_utils_version"]
 if __version__ > needed_utils_version:
     raise errors.CogLoadError("The needed utils to run the cog has a higher version than the one supported by this version of the cog. Please update the cogs of the `AAA3A-cogs` repo.")
 elif __version__ < needed_utils_version:
-    raise errors.CogLoadError("The needed utils to run the cog has a lower version than the one supported by this version of cog. Please execute the command `[p]pipinstall git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot might be necessary.")
+    raise errors.CogLoadError("The needed utils to run the cog has a lower version than the one supported by this version of cog. Please execute the command `[p]pipinstall --upgrade git+https://github.com/AAA3A-AAA3A/AAA3A_utils.git`. A restart of the bot might be necessary.")
 
 from redbot.core.bot import Red  # isort:skip
 import json

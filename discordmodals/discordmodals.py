6634f19c921a08d63287eb1a3223ab3423a78339
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-05-14 14:25:54 +0200

    [DiscordModals] Remove the use of the Views utils.

--- discordmodals/discordmodals.py
+++ discordmodals/discordmodals.py
@@ -1,4 +1,4 @@
-from AAA3A_utils import Cog, CogsUtils, Buttons, Modal  # isort:skip
+from AAA3A_utils import Cog, CogsUtils  # isort:skip
 from redbot.core import commands, Config  # isort:skip
 from redbot.core.bot import Red  # isort:skip
 from redbot.core.i18n import Translator, cog_i18n  # isort:skip
@@ -6,6 +6,9 @@ import discord  # isort:skip
 import typing  # isort:skip
 
 import yaml
+
+from copy import deepcopy
+from functools import partial
 
 # Credits:
 # General repo credits.
@@ -49,7 +52,7 @@ class YAMLConverter(commands.Converter):
                     _("The argument `/button/{arg}` is required in the YAML.").format(arg=arg)
                 )
         for arg in argument_dict["button"]:
-            if arg is not None and arg not in required_arguments + optional_arguments:
+            if arg not in required_arguments + optional_arguments:
                 raise discord.ext.commands.BadArgument(
                     _(
                         "The agument `/button/{arg}` is invalid in the YAML. Check the spelling."
@@ -76,7 +79,7 @@ class YAMLConverter(commands.Converter):
                 _("The argument `/button/modal` must be a list of TextInputs.")
             )
         required_arguments = ["label"]
-        optional_arguments = ["style", "required", "default", "placeholder"]
+        optional_arguments = ["style", "required", "default", "placeholder", "min_length", "max_length"]
         for count, input in enumerate(argument_dict["modal"], start=1):
             count += 1
             for arg in required_arguments:
@@ -87,7 +90,7 @@ class YAMLConverter(commands.Converter):
                         )
                     )
             for arg in input:
-                if arg is not None and arg not in required_arguments + optional_arguments:
+                if arg not in required_arguments + optional_arguments:
                     raise discord.ext.commands.BadArgument(
                         _(
                             "The agument `/modal/{count}/{arg}` is invalid in the YAML. Check the spelling."
@@ -161,10 +164,10 @@ class YAMLConverter(commands.Converter):
         if "messages" in argument_dict:
             if "error" not in argument_dict["messages"]:
                 argument_dict["messages"]["error"] = None
-            if "done" not in argument_dict["messages"]:
-                argument_dict["messages"]["done"] = None
+            if "submit" not in argument_dict["messages"]:
+                argument_dict["messages"]["submit"] = None
         else:
-            argument_dict["messages"] = {"error": None, "done": None}
+            argument_dict["messages"] = {"error": None, "submit": None}
         return argument_dict
 
 
@@ -180,17 +183,58 @@ class DiscordModals(Cog):
             identifier=205192943327321000143939875896557571750,  # 897374386384
             force_registration=True,
         )
+        self.CONFIG_SCHEMA: int = 2
+        self.discordmodals_global: typing.Dict[str, typing.Optional[int]] = {
+            "CONFIG_SCHEMA": None,
+        }
         self.discordmodals_guild: typing.Dict[
-            str, typing.Dict[str, typing.Dict[str, typing.Any]]
+            str, typing.Dict[str, typing.Union[str, typing.Dict[str, typing.Any]]]
         ] = {
             "modals": {},
         }
+        self.config.register_global(**self.discordmodals_global)
         self.config.register_guild(**self.discordmodals_guild)
 
         self.cogsutils: CogsUtils = CogsUtils(cog=self)
 
     async def cog_load(self) -> None:
+        await self.edit_config_schema()
         await self.load_buttons()
+
+    async def edit_config_schema(self) -> None:
+        CONFIG_SCHEMA = await self.config.CONFIG_SCHEMA()
+        ALL_CONFIG_GLOBAL = await self.config.all()
+        if ALL_CONFIG_GLOBAL == self.config._defaults[self.config.GLOBAL]:
+            CONFIG_SCHEMA = self.CONFIG_SCHEMA
+            await self.config.CONFIG_SCHEMA.set(CONFIG_SCHEMA)
+            return
+        if CONFIG_SCHEMA is None:
+            CONFIG_SCHEMA = 1
+            await self.config.CONFIG_SCHEMA(CONFIG_SCHEMA)
+        if CONFIG_SCHEMA == self.CONFIG_SCHEMA:
+            return
+        if CONFIG_SCHEMA == 1:
+            guild_group = self.config._get_base_group(self.config.GUILD)
+            async with guild_group.all() as guilds_data:
+                _guilds_data = deepcopy(guilds_data)
+                for guild in _guilds_data:
+                    if "submit" in list(guilds_data[guild]["modals"].values())[0]["messages"]:
+                        break
+                    for modal in guilds_data[guild]["modals"]:
+                        guilds_data[guild]["modals"][modal]["messages"]["submit"] = guilds_data[guild]["modals"][modal]["messages"]["done"]
+                        del guilds_data[guild]["modals"][modal]["messages"]["done"]
+                        guilds_data[guild]["modals"][modal]["button"] = guilds_data[guild]["modals"][modal]["button"]["buttons"][0]
+                        for key in ["members", "check", "function", "function_args"]:
+                            if key in guilds_data[guild]["modals"][modal]["modal"]:
+                                del guilds_data[guild]["modals"][modal]["modal"][key]
+            CONFIG_SCHEMA = 2
+            await self.config.CONFIG_SCHEMA.set(CONFIG_SCHEMA)
+        if CONFIG_SCHEMA < self.CONFIG_SCHEMA:
+            CONFIG_SCHEMA = self.CONFIG_SCHEMA
+            await self.config.CONFIG_SCHEMA.set(CONFIG_SCHEMA)
+        self.log.info(
+            f"The Config schema has been successfully modified to {self.CONFIG_SCHEMA} for the {self.qualified_name} cog."
+        )
 
     async def load_buttons(self) -> None:
         all_guilds = await self.config.all_guilds()
@@ -198,8 +242,13 @@ class DiscordModals(Cog):
             for modal in all_guilds[guild]["modals"]:
                 try:
                     button = all_guilds[guild]["modals"][modal]["button"]
-                    button["function"] = self.send_modal
-                    view = Buttons.from_dict_cogsutils(button)
+                    if "custom_id" not in button:
+                        button["custom_id"] = f"DiscordModals_{self.cogsutils.generate_key(length=10)}"
+                    button["style"] = discord.ButtonStyle(button["style"])
+                    button = discord.ui.Button(**button)
+                    button.callback = self.send_modal
+                    view = discord.ui.View(timeout=None)
+                    view.add_item(button)
                     self.bot.add_view(view, message_id=int((str(modal).split("-"))[1]))
                     self.cogsutils.views.append(view)
                 except Exception as e:
@@ -208,14 +257,25 @@ class DiscordModals(Cog):
                         exc_info=e,
                     )
 
-    async def send_modal(self, view: Buttons, interaction: discord.Interaction) -> None:
+    async def send_modal(self, interaction: discord.Interaction) -> None:
         config = await self.config.guild(interaction.message.guild).modals()
         if f"{interaction.message.channel.id}-{interaction.message.id}" not in config:
             return
         try:
-            modal = config[f"{interaction.message.channel.id}-{interaction.message.id}"]["modal"]
-            modal["function"] = self.send_embed_with_responses
-            await interaction.response.send_modal(Modal.from_dict_cogsutils(modal))
+            modal_config = config[f"{interaction.message.channel.id}-{interaction.message.id}"]["modal"]
+            modal = discord.ui.Modal(
+                title=modal_config["title"],
+                timeout=modal_config["timeout"],
+                custom_id=modal_config["custom_id"],
+            )
+            inputs = []
+            for _input in modal_config["inputs"]:
+                _input["style"] = discord.TextStyle(_input["style"])
+                text_input = discord.ui.TextInput(**_input)
+                inputs.append(text_input)
+                modal.add_item(text_input)
+            modal.on_submit = partial(self.send_embed_with_responses, inputs=inputs)
+            await interaction.response.send_modal(modal)
         except Exception as e:
             self.log.error(
                 f"The Modal of the {interaction.message.guild.id}-{interaction.message.channel.id}-{interaction.message.id} message did not work properly.",
@@ -226,9 +286,7 @@ class DiscordModals(Cog):
                     _("Sorry. An error has occurred."), ephemeral=True
                 )
 
-    async def send_embed_with_responses(
-        self, view: Modal, interaction: discord.Interaction, values: typing.List
-    ) -> None:
+    async def send_embed_with_responses(self, interaction: discord.Interaction, inputs: typing.List[discord.ui.TextInput]) -> None:
         config = await self.config.guild(interaction.message.guild).modals()
         if f"{interaction.message.channel.id}-{interaction.message.id}" not in config:
             return
@@ -267,11 +325,9 @@ class DiscordModals(Cog):
                     name="Anonymous",
                     icon_url="https://forum.mtasa.com/uploads/monthly_2016_10/Anonyme.png.4060431ce866962fa496657f752d5613.png",
                 )
-            for value in values:
-                if not hasattr(value, "label") or not hasattr(value, "value"):
-                    continue
+            for _input in inputs:
                 try:
-                    embed.add_field(name=value.label, value=value.value, inline=False)
+                    embed.add_field(name=_input.label, value=_input.value.strip() or "Not provided.", inline=False)
                 except Exception:
                     pass
             embed.set_footer(text=interaction.guild.name, icon_url=interaction.guild.icon)
@@ -286,7 +342,7 @@ class DiscordModals(Cog):
             )
         else:
             await interaction.followup.send(
-                config["messages"]["done"] or _("Thank you for sending this Modal!"),
+                config["messages"]["submit"] or _("Thank you for sending this Modal!"),
                 ephemeral=True,
             )
 
@@ -334,7 +390,7 @@ class DiscordModals(Cog):
         anonymous: False
         messages:
           error: Error!
-          done: Form submitted.
+          submit: Form submitted.
         ```
         The `emoji`, `style`, `required`, `default`, `placeholder`, `min_length`, `max_length`, `channel`, `anonymous` and `messages` are not required.
         """
@@ -346,33 +402,41 @@ class DiscordModals(Cog):
         if f"{message.channel.id}-{message.id}" in config:
             raise commands.UserFeedbackCheckFailure(_("This message already has a Modal."))
         try:
-            argument["button"][
-                "custom_id"
-            ] = f"DiscordModals_{self.cogsutils.generate_key(length=10)}"
-            view = Buttons(
-                timeout=None, buttons=[argument["button"]], function=self.send_modal, infinity=True
-            )
+            button = argument["button"]
+            button["style"] = discord.ButtonStyle(button["style"])
+            button["custom_id"] = f"DiscordModals_{self.cogsutils.generate_key(length=10)}"
+            button = discord.ui.Button(**button)
+            button.callback = self.send_modal
+            view = discord.ui.View(timeout=None)
+            view.add_item(button)
             await message.edit(view=view)
             self.cogsutils.views.append(view)
         except discord.HTTPException:
             raise commands.UserFeedbackCheckFailure(
                 _("Sorry. An error occurred when I tried to put the button on the message.")
             )
-        modal = Modal(
+        modal = discord.ui.Modal(
             title=argument["title"],
-            inputs=argument["modal"],
-            function=self.send_embed_with_responses,
+            timeout=None,
             custom_id=f"DiscordModals_{self.cogsutils.generate_key(length=10)}",
         )
+        inputs = []
+        for _input in argument["modal"]:
+            _input = _input.copy()
+            _input["style"] = discord.TextStyle(_input["style"])
+            text_input = discord.ui.TextInput(**_input)
+            inputs.append(text_input)
+            modal.add_item(text_input)
+        modal.on_submit = partial(self.send_embed_with_responses, inputs=inputs)
         config[f"{message.channel.id}-{message.id}"] = {
             "title": argument["title"],
-            "button": view.to_dict_cogsutils(True),
+            "button": {"style": button.style.value, "label": button.label, "emoji": str(button.emoji), "custom_id": button.custom_id},
             "channel": argument["channel"],
-            "modal": modal.to_dict_cogsutils(True),
+            "modal": {"title": modal.title, "timeout": modal.timeout, "custom_id": modal.custom_id, "inputs": argument["modal"]},
             "anonymous": argument["anonymous"],
             "messages": {
                 "error": argument["messages"]["error"],
-                "done": argument["messages"]["done"],
+                "submit": argument["messages"]["submit"],
             },
         }
         await self.config.guild(ctx.guild).modals.set(config)

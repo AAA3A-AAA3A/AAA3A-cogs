17ae220f27cc596f657698c0174f89c65eee3ba7
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-06-10 19:19:54 +0200

    [DiscordEdit] Add `[p]editrole hoist` and `[p]editrole displayicon` commands.

--- discordedit/editrole.py
+++ discordedit/editrole.py
@@ -5,7 +5,14 @@ from redbot.core.bot import Red  # isort:skip
 import discord  # isort:skip
 import typing  # isort:skip
 
+import aiohttp
+
 from redbot.core.utils.chat_formatting import box, pagify
+
+try:
+    from emoji import UNICODE_EMOJI_ENGLISH as EMOJI_DATA  # emoji<2.0.0
+except ImportError:
+    from emoji import EMOJI_DATA  # emoji>=2.0.0
 
 def _(untranslated: str) -> str:  # `redgettext` will found these strings.
     return untranslated
@@ -67,16 +74,14 @@ class EditRole(Cog):
         ):
             raise commands.UserFeedbackCheckFailure(
                 _(
-                    "I can not let you edit {role.mention} ({role.id}) because that role is higher than or equal to your highest role in the Discord hierarchy."
+                    "I can not let you edit @{role.name} ({role.id}) because that role is higher than or equal to your highest role in the Discord hierarchy."
                 ).format(role=role),
-                allowed_mentions=None,
             )
         if not ctx.guild.me.top_role > role:
             raise commands.UserFeedbackCheckFailure(
                 _(
-                    "I can not edit {role.mention} ({role.id}) because that role is higher than or equal to my highest role in the Discord hierarchy."
+                    "I can not edit @{role.name} ({role.id}) because that role is higher than or equal to my highest role in the Discord hierarchy."
                 ).format(role=role),
-                allowed_mentions=None,
             )
         return True
 
@@ -151,6 +156,78 @@ class EditRole(Cog):
         try:
             await role.edit(
                 color=color,
+                reason=f"{ctx.author} ({ctx.author.id}) has modified the role {role.name} ({role.id}).",
+            )
+        except discord.HTTPException as e:
+            raise commands.UserFeedbackCheckFailure(
+                _(ERROR_MESSAGE).format(error=box(e, lang="py"))
+            )
+
+    @editrole.command(name="hoist")
+    async def editrole_hoist(
+        self, ctx: commands.Context, role: discord.Role, hoist: bool
+    ) -> None:
+        """Edit role hoist."""
+        await self.check_role(ctx, role)
+        try:
+            await role.edit(
+                hoist=hoist,
+                reason=f"{ctx.author} ({ctx.author.id}) has modified the role {role.name} ({role.id}).",
+            )
+        except discord.HTTPException as e:
+            raise commands.UserFeedbackCheckFailure(
+                _(ERROR_MESSAGE).format(error=box(e, lang="py"))
+            )
+
+    class EmojiOrUrlConverter(commands.Converter):
+        async def convert(self, ctx: commands.Context, argument: str):
+            try:
+                return await discord.ext.commands.converter.CONVERTER_MAPPING[discord.Emoji]().convert(
+                    ctx, argument
+                )
+            except commands.BadArgument:
+                pass
+            if argument.startswith("<") and argument.endswith(">"):
+                argument = argument[1:-1]
+            return argument
+
+    @editrole.command(name="displayicon", aliases=["icon"])
+    async def editrole_display_icon(
+        self, ctx: commands.Context, role: discord.Role, display_icon: typing.Optional[EmojiOrUrlConverter] = None
+    ) -> None:
+        """Edit role display icon.
+
+        `display_icon` can a Unicode emoji, a custom emoji or an url. You can also upload an attachment.
+        """
+        if "ROLE_ICONS" not in ctx.guild.features:
+            raise commands.UserFeedbackCheckFailure(_("This server doesn't have `ROLE_ICONS` feature. This server needs more boosts to perform this action."))
+        await self.check_role(ctx, role)
+        if len(ctx.message.attachments) > 0:
+            display_icon = await ctx.message.attachments[0].read()  # Read an optional attachment.
+        elif display_icon is not None:
+            if isinstance(display_icon, discord.Emoji):
+                emoji_url = f"https://cdn.discordapp.com/emojis/{display_icon.id}.png"
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(emoji_url) as r:
+                        display_icon = await r.read()  # Get emoji data.
+            elif display_icon.strip("\N{VARIATION SELECTOR-16}") in EMOJI_DATA:
+                display_icon = display_icon
+            else:
+                url = display_icon
+                async with aiohttp.ClientSession() as session:
+                    try:
+                        async with session.get(url) as r:
+                            display_icon = await r.read()  # Get URL data.
+                    except aiohttp.InvalidURL:
+                        return await ctx.send("That URL is invalid.")
+                    except aiohttp.ClientError:
+                        return await ctx.send("Something went wrong while trying to get the image.")
+        else:
+            await ctx.send_help()  # Send the command help if no attachment, no Unicode/custom emoji and no URL.
+            return
+        try:
+            await role.edit(
+                display_icon=display_icon,
                 reason=f"{ctx.author} ({ctx.author.id}) has modified the role {role.name} ({role.id}).",
             )
         except discord.HTTPException as e:

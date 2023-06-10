c20c40e231334d157f6f40d5eb63f2c3189ec4aa
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-06-10 18:37:47 +0200

    Update converters.py

--- reminders/converters.py
+++ reminders/converters.py
@@ -400,7 +400,7 @@ class TimeConverter(commands.Converter):
             info = "\n".join(info)
             raise commands.BadArgument(f"Error(s) during parsing the input:\n{info}")
         if text is not None:
-            if text.split(" ")[0].lower() == "to":
+            if text.split(" ")[0].lower() == "to" and text.split(" ")[1].lower() != "do":
                 text = text[2:]
             elif text.split(" ")[0].lower() == "that":
                 text = text[4:]

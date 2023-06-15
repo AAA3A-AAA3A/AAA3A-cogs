f478c8e3456f8f2b96f0510836abaffd891afbbd
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-06-15 19:37:37 +0200

    [MemberPrefix] Remove the `use_normal_prefixes` setting.

--- memberprefix/memberprefix.py
+++ memberprefix/memberprefix.py
@@ -1,6 +1,6 @@
 from AAA3A_utils import Cog, CogsUtils, Settings  # isort:skip
 from redbot.core import commands, Config  # isort:skip
-from redbot.core.bot import Red  # isort:skip
+from redbot.core.bot import Red, NotMessage  # isort:skip
 from redbot.core.i18n import Translator, cog_i18n  # isort:skip
 import discord  # isort:skip
 import typing  # isort:skip
@@ -28,7 +28,7 @@ class MemberPrefix(Cog):
             force_registration=True,
         )
         self.memberprefix_global: typing.Dict[str, bool] = {
-            "use_normal_prefixes": False,
+            "use_normal_prefixes": True,
         }
         self.memberprefix_member: typing.Dict[str, typing.List[str]] = {
             "custom_prefixes": [],
@@ -40,29 +40,29 @@ class MemberPrefix(Cog):
 
         self.cogsutils: CogsUtils = CogsUtils(cog=self)
 
-        _settings: typing.Dict[
-            str, typing.Dict[str, typing.Union[typing.List[str], bool, str]]
-        ] = {
-            "use_normal_prefixes": {
-                "path": ["use_normal_prefixes"],
-                "converter": bool,
-                "description": "Use server/global prefixes in addition to those customized for each member.",
-            },
-        }
-        self.settings: Settings = Settings(
-            bot=self.bot,
-            cog=self,
-            config=self.config,
-            group=self.config.GLOBAL,
-            settings=_settings,
-            global_path=[],
-            use_profiles_system=False,
-            can_edit=True,
-            commands_group=self.configuration,
-        )
+        # _settings: typing.Dict[
+        #     str, typing.Dict[str, typing.Union[typing.List[str], bool, str]]
+        # ] = {
+        #     "use_normal_prefixes": {
+        #         "path": ["use_normal_prefixes"],
+        #         "converter": bool,
+        #         "description": "Use server/global prefixes in addition to those customized for each member.",
+        #     },
+        # }
+        # self.settings: Settings = Settings(
+        #     bot=self.bot,
+        #     cog=self,
+        #     config=self.config,
+        #     group=self.config.GLOBAL,
+        #     settings=_settings,
+        #     global_path=[],
+        #     use_profiles_system=False,
+        #     can_edit=True,
+        #     commands_group=self.configuration,
+        # )
 
     async def cog_load(self) -> None:
-        await self.settings.add_commands()
+        # await self.settings.add_commands()
         self.bot.command_prefix = self.prefix_manager
 
     async def cog_unload(self) -> None:
@@ -114,24 +114,17 @@ class MemberPrefix(Cog):
         file = io.BytesIO(str(data).encode(encoding="utf-8"))
         return {f"{self.qualified_name}.json": file}
 
-    async def prefix_manager(self, bot: Red, message: discord.Message) -> typing.List[str]:
-        if message.guild is None or await bot.cog_disabled_in_guild(cog=self, guild=message.guild) or not await bot.allowed_by_whitelist_blacklist(who=message.author):
-            prefixes = await bot._prefix_cache.get_prefixes(message.guild)
-            if bot._cli_flags.mentionable:
-                return discord.ext.commands.when_mentioned_or(*prefixes)(bot, message)
-            return prefixes
+    async def prefix_manager(self, bot: Red, message: typing.Union[discord.Message, NotMessage]) -> typing.List[str]:
+        if not isinstance(message, discord.Message) or message.guild is None or await bot.cog_disabled_in_guild(cog=self, guild=message.guild) or not await bot.allowed_by_whitelist_blacklist(who=message.author):
+            return await self.original_prefix_manager(bot, message)
         custom_prefixes = await self.config.member_from_ids(message.guild.id, message.author.id).custom_prefixes()
         if custom_prefixes == []:
+            return await self.original_prefix_manager(bot, message)
+        if await self.config.use_normal_prefixes():  # Always `True` because the setting has been removed.
             prefixes = await bot._prefix_cache.get_prefixes(message.guild)
-            if bot._cli_flags.mentionable:
-                return discord.ext.commands.when_mentioned_or(*prefixes)(bot, message)
-        else:
-            if await self.config.use_normal_prefixes():
-                prefixes = await bot._prefix_cache.get_prefixes(message.guild)
-            prefixes.extend(custom_prefixes)
-            prefixes = sorted(prefixes, reverse=True)
-            return discord.ext.commands.when_mentioned_or(*prefixes)(bot, message)
-        return prefixes
+        prefixes.extend(custom_prefixes)
+        prefixes = sorted(prefixes, reverse=True)
+        return discord.ext.commands.when_mentioned_or(*prefixes)(bot, message)
 
     class StrConverter(commands.Converter):
         async def convert(self, ctx: commands.Context, argument: str) -> str:
@@ -169,9 +162,9 @@ class MemberPrefix(Cog):
             )
         await self.config.member(ctx.author).custom_prefixes.set(prefixes)
         if len(prefixes) == 1:
-            await ctx.send(_("Prefix for you only set. You can mention the bot to invoke a command, or reinitialize your prefixes with the same command if you forget them."))
+            await ctx.send(_("Prefix for you only set. You can use current prefixes or mention the bot to invoke a command, or reinitialize your prefixes with the same command if you forget them."))
         else:
-            await ctx.send(_("Prefixes for you only set. You can mention the bot to invoke a command, or reinitialize your prefixes with the same command if you forget them."))
+            await ctx.send(_("Prefixes for you only set. You can use current prefixes or mention the bot to invoke a command, or reinitialize your prefixes with the same command if you forget them."))
 
     @commands.is_owner()
     @commands.guild_only()

09b0ee59d29951fe1afcc3201dc1c6be04b42ec4
fakesmile <69390477+fakesmile9704@users.noreply.github.com>
2023-08-10 00:42:09 +0530

    Update reminders.py

--- reminders/reminders.py
+++ reminders/reminders.py
@@ -847,10 +847,9 @@ class Reminders(Cog):
     @reminder.command()
     async def timezone(self, ctx: commands.Context, timezone: TimezoneConverter) -> None:
         """Set your timezone for the time converter.
-
+        
         Timezone should be specified in the format: `Continent/City`.
         Example timezones: `Europe/Paris`, `America/New_York`...
-        
         You can find a list of valid timezones at: https://timezonedb.com/time-zones
         """
         await self.config.user(ctx.author).timezone.set(timezone)

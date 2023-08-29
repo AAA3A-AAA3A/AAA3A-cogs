4546c7a31e37dfe58fc03a9ce0cc7bb06540e8a5
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-08-29 13:43:54 +0200

    [EditFile] Add `[p]editfile treedir`.

--- editfile/editfile.py
+++ editfile/editfile.py
@@ -8,6 +8,7 @@ import inspect
 import io
 import os
 import re
+import textwrap
 import typing
 from os import listdir
 from pathlib import Path
@@ -250,6 +251,33 @@ class EditFile(Cog):
             elif path_file.is_dir():
                 message += "\n" + f"- [DIR] {file}"
         message = CogsUtils.replace_var_paths(message)
+        await Menu(pages=message, lang="ini").start(ctx)
+
+    @editfile.command()
+    async def treedir(self, ctx: commands.Context, *, path: str) -> None:
+        """Make a tree with all files/directories of a directory from its path."""
+        path = Path(CogsUtils.replace_var_paths(path, reverse=True))
+        if not path.exists():
+            raise commands.UserFeedbackCheckFailure(
+                _("This directory cannot be found on the host machine.")
+            )
+        if not path.is_dir():
+            raise commands.UserFeedbackCheckFailure(
+                _("The path you specified refers to a file, not a directory.")
+            )
+        def tree(base):
+            lines = []
+            files = sorted(base.iterdir(), key=lambda s: s.name.lower())
+            for num, path in enumerate(files, start=1):
+                prefix = "└── " if num == len(files) else "├── "
+                if path.name.startswith('.') or {"venv", "__pycache__"} & {p.name for p in path.parents}:
+                    continue
+                lines.append(prefix + path.name)
+                if path.is_dir():
+                    indent = "   " if num == len(files) else "|   "
+                    lines.append(textwrap.indent(tree(path), prefix=indent))
+            return "\n".join(lines)
+        message = CogsUtils.replace_var_paths(tree(path))
         await Menu(pages=message, lang="ini").start(ctx)
 
     @editfile.command()

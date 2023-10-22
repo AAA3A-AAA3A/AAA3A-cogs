2aebd751cf3403bf680edfb3cf533cee8c0ed108
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2023-10-22 03:04:43 +0200

    Update linkquoter.py

--- linkquoter/linkquoter.py
+++ linkquoter/linkquoter.py
@@ -276,18 +276,18 @@ class LinkQuoter(Cog):
     async def migratefromlinkquoter(self, ctx: commands.Context) -> None:
         """Migrate config from LinkQuoter by Phen."""
         old_config: Config = Config.get_conf(
-            "LinkQuoter", identifier=6234567898747434823, force_registration=True, cog_name="Seen"
+            "LinkQuoter", identifier=6234567898747434823, force_registration=True, cog_name="LinkQuoter"
         )
         new_guild_group = self.config._get_base_group(self.config.GUILD)
         old_guilds_data = await old_config.all_members()
         async with new_guild_group.all() as new_guilds_data:
             for guild_id in old_guilds_data:
-                if old_guilds_data[guild_id].get("on") is not None:
+                if "on" in old_guilds_data[guild_id]:
                     new_guilds_data[str(guild_id)]["enabled"] = old_guilds_data[guild_id]["on"]
-                if old_guilds_data[guild_id].get("webhooks") is not None:
+                if "webhooks" in old_guilds_data[guild_id]:
                     new_guilds_data[str(guild_id)]["webhooks"] = old_guilds_data[guild_id]["webhooks"]
-                if old_guilds_data[guild_id].get("cross_server") is not None:
+                if "cross_server" in old_guilds_data[guild_id]:
                     new_guilds_data[str(guild_id)]["cross_server"] = old_guilds_data[guild_id]["cross_server"]
-                if old_guilds_data[guild_id].get("delete") is not None:
+                if "delete" in old_guilds_data[guild_id]:
                     new_guilds_data[str(guild_id)]["delete_message"] = old_guilds_data[guild_id]["delete_message"]
         await ctx.send(_("Data successfully migrated from LinkQuoter by Phen."))

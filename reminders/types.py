e5398ea50e4778851152c5e5cc2b1ddf59103d9f
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-04-10 11:34:52 +0200

    [Reminders] Limit the length of a command in `[p]reminder list` (fields limited to 1024 characters).

--- reminders/types.py
+++ reminders/types.py
@@ -513,7 +513,7 @@ class Reminder:
                     else (
                         f"Message {self.content['message_jump_url']}."
                         if self.content["type"] == "message"
-                        else f"Command `[p]{self.content['command']}` executed with your privilege rights."
+                        else "Command `[p]" + (f"{self.content['command'][:175]}..." if len(self.content["command"]) > 175 else self.content["command"]) + "` executed with your privilege rights."
                     )
                 )
             ),

48e0b3fe78575c0bf11344b4dea4ca0da68a2fd2
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-05-20 16:47:20 +0200

    [Dashboard] Improve the `Pagination` system.

--- dashboard/rpc/pagination.py
+++ dashboard/rpc/pagination.py
@@ -4,13 +4,16 @@ import typing  # isort:skip
 class Pagination(typing.List):
     """Pagination class for lists."""
 
-    DEFAULT_PER_PAGE = 20
+    DEFAULT_PER_PAGE: int = 20
+    DEFAULT_PAGE: int = 1
 
     def __init__(self, *args, **kwargs) -> None:
         self.total: int = kwargs.pop("total", None)
         self.per_page: int = kwargs.pop("per_page", None)
         self.pages: int = kwargs.pop("pages", None)
         self.page: int = kwargs.pop("page", None)
+        self.default_per_page: int = kwargs.pop("default_per_page", self.DEFAULT_PER_PAGE)
+        self.default_page: int = kwargs.pop("default_page", 1)
         super().__init__(*args, **kwargs)
 
     def to_dict(self) -> typing.Dict[str, typing.Any]:
@@ -20,6 +23,8 @@ class Pagination(typing.List):
             "per_page": self.per_page,
             "pages": self.pages,
             "page": self.page,
+            "default_per_page": self.default_per_page,
+            "default_page": self.default_page,
         }
 
     def __repr__(self) -> str:
@@ -46,7 +51,7 @@ class Pagination(typing.List):
         per_page: typing.Optional[typing.Union[int, str]] = None,
         page: typing.Optional[typing.Union[int, str]] = None,
         default_per_page: int = DEFAULT_PER_PAGE,
-        default_page: int = 1,
+        default_page: int = DEFAULT_PAGE,
     ) -> typing.Any:
         per_page = (
             default_per_page

9a3cf237061222611e47431cf443029d9aad7689
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-07-23 22:40:17 +0200

    Revert "Update third_parties.py"
    This reverts commit 325d85b3be59d4836b0ce4cd0dc7da9f0928bc5b.

--- dashboard/rpc/third_parties.py
+++ dashboard/rpc/third_parties.py
@@ -150,25 +150,28 @@ class DashboardRPC_ThirdParties:
             raise RuntimeError(f"`{name}` is already an existing third party.")
         _pages = {}
         for attr in dir(cog):
-            if hasattr((func := getattr(cog, attr)), "__dashboard_decorator_params__"):
-                setattr(
-                    cog,
-                    attr,
-                    types.MethodType(
-                        dashboard_page(
-                            *func.__dashboard_decorator_params__[0],
-                            **func.__dashboard_decorator_params__[1],
-                        )(func.__func__),
-                        func.__self__,
-                    ),
-                )
-            if hasattr((func := getattr(cog, attr)), "__dashboard_params__"):
-                page = func.__dashboard_params__["name"]
-                if page in _pages:
-                    raise RuntimeError(
-                        f"The page {page} is already an existing page for this third party."
+            try:
+                if hasattr((func := getattr(cog, attr)), "__dashboard_decorator_params__"):
+                    setattr(
+                        cog,
+                        attr,
+                        types.MethodType(
+                            dashboard_page(
+                                *func.__dashboard_decorator_params__[0],
+                                **func.__dashboard_decorator_params__[1],
+                            )(func.__func__),
+                            func.__self__,
+                        ),
                     )
-                _pages[page] = (func, func.__dashboard_params__)
+                if hasattr((func := getattr(cog, attr)), "__dashboard_params__"):
+                    page = func.__dashboard_params__["name"]
+                    if page in _pages:
+                        raise RuntimeError(
+                            f"The page {page} is already an existing page for this third party."
+                        )
+                    _pages[page] = (func, func.__dashboard_params__)
+            except TypeError:
+                continue
         if not _pages:
             raise RuntimeError("No page found.")
         self.third_parties[name] = _pages

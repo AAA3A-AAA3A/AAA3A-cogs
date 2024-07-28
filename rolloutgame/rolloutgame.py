bc204c56b5e75d9df7b2d78a7cd05c256e8a29cc
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-07-28 19:50:24 +0200

    Update rolloutgame.py

--- rolloutgame/rolloutgame.py
+++ rolloutgame/rolloutgame.py
@@ -164,7 +164,7 @@ class RolloutGame(Cog):
                 ]
             )
         for page in pagify(str(table), page_length=2000):
-            embed = discord.Embed(title="MemoryGame Leaderboard")
+            embed = discord.Embed(title="RolloutGame Leaderboard")
             embed.description = box(page, lang="py")
             if you:
                 embed.set_footer(text=you)

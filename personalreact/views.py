dc89f48fbc83d50595c5a367b6186d533a22e6f6
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-08-16 17:23:17 +0200

    Update views.py

--- personalreact/views.py
+++ personalreact/views.py
@@ -96,7 +96,7 @@ class PersonalReactView(discord.ui.View):
             name=_("Custom Trigger Roles Requirements:"),
             value=(
                 (_("**+ ∞** Staff") if custom_trigger_is_staff else "")
-                + (_("\n**+ {base_total_amount}** Always Allow").format(base_total_amount=base_total_amount) if always_allow_custom_trigger and base_total_amount > 0 else "")
+                + (_("\n**{action} {base_total_amount}** Always Allow").format(action="+" if base_use_amounts_sum else "•", base_total_amount=base_total_amount) if always_allow_custom_trigger and base_total_amount > 0 else "")
                 + ("\n" if custom_trigger_roles_requirements else "")
                 + "\n".join(
                     f"**{'+' if custom_trigger_use_amounts_sum else '•'} {amount}** — {role.mention}"

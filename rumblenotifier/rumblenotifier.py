ad81062e689007719cceb375f8f405068f49d20b
AAA3A <89632044+AAA3A-AAA3A@users.noreply.github.com>
2024-09-06 21:18:34 +0200

    [RumbleNotifier] New cog!

---
+++ rumblenotifier/rumblenotifier.py
@@ -0,0 +1,137 @@
+from AAA3A_utils import Cog, Settings  # isort:skip
+from redbot.core import commands, Config  # isort:skip
+from redbot.core.bot import Red  # isort:skip
+from redbot.core.i18n import Translator, cog_i18n  # isort:skip
+import discord  # isort:skip
+import typing  # isort:skip
+
+from .views import RumbleNotifierView
+
+# Credits:
+# General repo credits.
+
+_: Translator = Translator("RumbleNotifier", __file__)
+
+class RoleConverter(commands.RoleConverter):
+    async def convert(self, ctx: commands.Context, argument: str) -> discord.Role:
+        role = await super().convert(ctx, argument)
+        if role >= ctx.author.top_role and ctx.author.id not in ctx.bot.owner_ids:
+            raise commands.BadArgument(_("You can't set a role higher than your own."))
+        return role
+
+
+@cog_i18n(_)
+class RumbleNotifier(Cog):
+    """Ping a role when a rumble starts, and let members suscribe or unsuscribe from the notifications!"""
+
+    def __init__(self, bot: Red) -> None:
+        super().__init__(bot=bot)
+
+        self.config: Config = Config.get_conf(
+            self,
+            identifier=205192943327321000143939875896557571750,
+            force_registration=True,
+        )
+        self.config.register_guild(
+            enabled=False,
+            channels=[],
+            role=None,
+            suscribing=False,
+        )
+
+        _settings: typing.Dict[
+            str, typing.Dict[str, typing.Union[typing.List[str], bool, str]]
+        ] = {
+            "enabled": {
+                "converter": bool,
+                "description": "Enable or disable the cog.",
+            },
+            "channels": {
+                "converter": commands.Greedy[typing.Union[discord.TextChannel, discord.VoiceChannel, discord.CategoryChannel]],
+                "description": "The channels/categories where the cog will detect rumbles.",
+            },
+            "role": {
+                "converter": RoleConverter,
+                "description": "The role that will be pinged when a rumble starts.",
+            },
+            "suscribing": {
+                "converter": bool,
+                "description": "Enable or disable the suscribing system.",
+            },
+        }
+        self.settings: Settings = Settings(
+            bot=self.bot,
+            cog=self,
+            config=self.config,
+            group=self.config.GUILD,
+            settings=_settings,
+            global_path=[],
+            use_profiles_system=False,
+            can_edit=True,
+            commands_group=self.setrumblenotifier,
+        )
+
+    async def cog_load(self) -> None:
+        await super().cog_load()
+        await self.settings.add_commands()
+        view: RumbleNotifierView = RumbleNotifierView(self)
+        self.bot.add_view(view)
+        self.views[None] = view
+
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message: discord.Message) -> None:
+        if message.guild is None:
+            return
+        if isinstance(message.channel, discord.Thread):
+            return
+        if not (
+            message.author.bot
+            and message.author.id == 693167035068317736
+            and message.embeds
+            and message.embeds[0].title is not None
+            and "Rumble Royale hosted" in message.embeds[0].title
+        ):
+            return
+        config = await self.config.guild(message.guild).all()
+        if (
+            await self.bot.cog_disabled_in_guild(cog=self, guild=message.guild)
+            or not await self.bot.allowed_by_whitelist_blacklist(who=message.author)
+            or not message.channel.permissions_for(message.guild.me).send_messages
+            or not config["enabled"]
+            or not (channels_ids := config["channels"])
+            or (message.channel.id not in channels_ids and message.channel.category_id not in channels_ids)
+            or (role_id := config["role"]) is None
+            or (role := message.guild.get_role(role_id)) is None
+        ):
+            return
+        suscribing = (
+            await self.config.guild(message.guild).suscribing()
+            and message.guild.me.guild_permissions.manage_roles
+            and role.is_assignable()
+            and role < message.guild.me.top_role
+        )
+        embed: discord.Embed = discord.Embed(
+            title=_("⚔️ New Rumble battle! ⚔️"),
+            description=_("I've notified the {length} members of the {role} role.").format(length=len(role.members), role=role.mention),
+            color=discord.Color.gold(),
+        )
+        if suscribing:
+            embed.add_field(
+                name="\u200b",
+                value=_("Want to be notified in the future? Use the buttons below!"),
+                inline=False,
+            )
+        embed.set_footer(text=message.guild.name, icon_url=message.guild.icon)
+        await message.channel.send(
+            content=role.mention,
+            embed=embed,
+            view=self.views[None] if suscribing else None,
+            allowed_mentions=discord.AllowedMentions(roles=True),
+        )
+
+    @commands.guild_only()
+    @commands.admin_or_permissions(manage_guild=True)
+    @commands.hybrid_group(aliases=["setrn"])
+    async def setrumblenotifier(self, ctx: commands.Context) -> None:
+        """Settings for the RumbleNotifier cog."""
+        pass
---
+++ rumblenotifier/views.py
@@ -0,0 +1,77 @@
+from redbot.core import commands  # isort:skip
+from redbot.core.i18n import Translator  # isort:skip
+import discord  # isort:skip
+
+from redbot.core.utils.chat_formatting import box
+
+_: Translator = Translator("RumbleNotifier", __file__)
+
+
+class RumbleNotifierView(discord.ui.View):
+    def __init__(self, cog: commands.Cog) -> None:
+        super().__init__(timeout=None)
+        self.cog: commands.Cog = cog
+
+        self.unsuscribe.label = _("Unsuscribe")
+        self.suscribe.label = _("Suscribe")
+
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        if (
+            await self.cog.bot.cog_disabled_in_guild(cog=self.cog, guild=interaction.guild)
+            or not await self.cog.bot.allowed_by_whitelist_blacklist(who=interaction.user)
+            or not await self.cog.config.guild(interaction.guild).enabled()
+            or not (channels_ids := await self.cog.config.guild(interaction.guild).channels())
+            or (interaction.channel.id not in channels_ids and interaction.channel.category_id not in channels_ids)
+            or (role_id := await self.cog.config.guild(interaction.guild).role()) is None
+            or (role := interaction.guild.get_role(role_id)) is None
+            or not await self.cog.config.guild(interaction.guild).suscribing()
+        ):
+            await interaction.response.send_message(
+                _("You are not allowed to use this interaction."), ephemeral=True
+            )
+            return False
+        if not (
+            interaction.guild.me.guild_permissions.manage_roles
+            and role.is_assignable()
+            and role < interaction.guild.me.top_role
+        ):
+            await interaction.response.send_message(
+                _("I don't have the required permissions to assign the role. Please contact an administrator."),
+            )
+            return False
+        return True
+
+    @discord.ui.button(emoji="✖️", label="Unsuscribe", style=discord.ButtonStyle.danger, custom_id="RumbleNotifier_unsuscribe")
+    async def unsuscribe(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        await interaction.response.defer(ephemeral=True, thinking=True)
+        role_id = await self.cog.config.guild(interaction.guild).role()
+        role = interaction.guild.get_role(role_id)
+        if role not in interaction.user.roles:
+            await interaction.followup.send(
+                _("You are already unsuscribed from the Rumble notifications."), ephemeral=True
+            )
+            return
+        try:
+            await interaction.user.remove_roles(role)
+        except discord.HTTPException as e:
+            await interaction.followup.send(
+                _("An error occurred while trying to remove the role. Please contact an administrator.\n") + box(str(e), lang="py"),
+            )
+        await interaction.followup.send(_("You have been removed from the Rumble notifications."), ephemeral=True)
+
+    @discord.ui.button(emoji="⚔️", label="Suscribe", style=discord.ButtonStyle.success, custom_id="RumbleNotifier_suscribe")
+    async def suscribe(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        role_id = await self.cog.config.guild(interaction.guild).role()
+        role = interaction.guild.get_role(role_id)
+        if role in interaction.user.roles:
+            await interaction.followup.send(
+                _("You are already suscribed to the Rumble notifications."), ephemeral=True
+            )
+            return
+        try:
+            await interaction.user.add_roles(role)
+        except discord.HTTPException as e:
+            await interaction.followup.send(
+                _("An error occurred while trying to add the role. Please contact an administrator.\n") + box(str(e), lang="py"),
+            )
+        await interaction.followup.send(_("You have been added to the Rumble notifications."), ephemeral=True)
